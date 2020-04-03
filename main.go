0f5317fa29c7b1d337daaef266d8b6c6ecc7babc
kplat1 <koreilly5297@gmail.com>
2020-04-03 15:37:33 -0700

    Post function

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/goki/mat32"
 	_ "github.com/heroku/x/hmetrics/onload"
-	// "strconv"
+	"strconv"
 )
 
 type PlayerPosData struct {
@@ -36,15 +36,14 @@ func main() {
 	router.GET("/website", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-	router.POST("/form_post", func(c *gin.Context) {
-		message := c.PostForm("message")
-		nick := c.DefaultPostForm("nick", "anonymous")
-
-		c.JSON(200, gin.H{
-			"status":  "posted",
-			"message": message,
-			"nick":    nick,
-		})
+	router.POST("/playerPosPost/:username/:battleName/:posX/:posY/:posZ/:points", func(c *gin.Context) {
+		points, _ := strconv.Atoi(c.Param("points"))
+		posX, _ := strconv.ParseFloat(c.Param("posX"), 32)
+		posY, _ := strconv.ParseFloat(c.Param("posY"), 32)
+		posZ, _ := strconv.ParseFloat(c.Param("posZ"), 32)
+		PlayerPos[c.Param("username")] = &PlayerPosData{c.Param("username"), c.Param("battleName"), mat32.Vec3{float32(posX), float32(posY), float32(posZ)}, points}
+		d := PlayerPos[c.Param("username")]
+		c.JSON(http.StatusOK, gin.H{"username": d.Username, "battleName": d.BattleName, "pos": d.Pos, "points": d.Points})
 	})
 
 	router.GET("/playerPosGet", func(c *gin.Context) {
