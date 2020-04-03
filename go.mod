d2cab9983af72a7373ee6927ce8838652465fa9d
kplat1 <koreilly5297@gmail.com>
2020-04-03 13:36:18 -0700

    Updated server

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.6.2
+	github.com/goki/mat32 v1.0.1
 	github.com/heroku/x v0.0.22
 )
--- main.go
+++ main.go
@@ -7,7 +7,17 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/goki/mat32"
 )
+
+type PlayerPosData struct {
+	Username   string
+	BattleName string
+	Pos        mat32.Vec3
+	Points     int
+}
+
+var PlayerPos map[string]*PlayerPosData
 
 func main() {
 	port := os.Getenv("PORT")
@@ -15,15 +25,25 @@ func main() {
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-
+	PlayerPos = make(map[string]*PlayerPosData)
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("webFiles/*.html")
 	router.Static("/static", "static")
+	PlayerPos["serverTest"] = &PlayerPosData{"serverTest", "testBattle", mat32.Vec3{1, 1, 1}, 5}
 
 	router.GET("/website", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
+
+	router.GET("/playerPos", func(c *gin.Context) {
+		for _, d := range PlayerPos {
+			c.JSON(http.StatusOK, gin.H{"username": d.Username, "battleName": d.BattleName, "pos": d.Pos, "points": d.Points})
+		}
+	})
+	// router.POST("/playerPos", func(c *gin.Context) {
+	//
+	// })
 
 	router.Run(":" + port)
 }
