fd666a1ae5d597a58b3bde8592069737c6f61341
kplat1 <koreilly5297@gmail.com>
2020-04-04 15:44:03 -0700

    Added weapons server!

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 	"os"
 
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/goki/mat32"
-	"encoding/json"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"sync"
 	// "io/ioutil"
@@ -17,13 +17,25 @@ import (
 type PlayerPosData struct {
 	Username   string
 	BattleName string
-	Pos mat32.Vec3
+	Pos        mat32.Vec3
 	Points     int
+}
+
+type FireEvent struct {
+	Creator string
+	Origin  mat32.Vec3
+	Dir     mat32.Vec3
+	Damage  int
+	BattleName string
 }
 
 type PlayerPosMap map[string]*PlayerPosData
 
+type FireEventMap map[int]*FireEvent
+
 var TheBattleMaps map[string]PlayerPosMap
+
+var TheFireEvents map[string]FireEventMap
 
 var ServerMutex sync.Mutex
 
@@ -34,6 +46,7 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 	TheBattleMaps = make(map[string]PlayerPosMap)
+	TheFireEvents = make(map[string]FireEventMap)
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("webFiles/*.html")
@@ -58,7 +71,7 @@ func main() {
 		// posY, _ := strconv.ParseFloat(c.Param("posY"), 32)
 		// posZ, _ := strconv.ParseFloat(c.Param("posZ"), 32)
 		ppmap, ok := TheBattleMaps[jsonStruct.BattleName]
-		if !ok || ppmap == nil{
+		if !ok || ppmap == nil {
 			ppmap = make(PlayerPosMap)
 			TheBattleMaps[jsonStruct.BattleName] = ppmap
 		}
@@ -85,6 +98,51 @@ func main() {
 			return
 		}
 		c.JSON(http.StatusOK, ppmap)
+		ServerMutex.Unlock()
+	})
+
+	router.POST("/fireEventsPost", func(c *gin.Context) {
+		ServerMutex.Lock()
+		jsonStruct := &FireEvent{}
+		decoder := json.NewDecoder(c.Request.Body)
+		err := decoder.Decode(jsonStruct)
+		if err != nil {
+			log.Printf("Err: %v \n", err)
+		}
+		log.Printf("Struct: %v", jsonStruct)
+		log.Printf("Battle Name: %v", jsonStruct.BattleName)
+		// points, _ := strconv.Atoi(c.Param("points"))
+		// posX, _ := strconv.ParseFloat(c.Param("posX"), 32)
+		// posY, _ := strconv.ParseFloat(c.Param("posY"), 32)
+		// posZ, _ := strconv.ParseFloat(c.Param("posZ"), 32)
+		femap, ok := TheFireEvents[jsonStruct.BattleName]
+		if !ok || femap == nil {
+			femap = make(FireEventMap)
+			TheFireEvents[jsonStruct.BattleName] = femap
+		}
+		femap[len(femap)] = jsonStruct
+		TheFireEvents[jsonStruct.BattleName] = femap
+		ServerMutex.Unlock()
+	})
+
+	router.GET("/fireEventsGet", func(c *gin.Context) {
+		ServerMutex.Lock()
+		battleName := c.Query("battleName")
+		if battleName == "" {
+			log.Printf("Didn't get battle name!")
+			c.String(422, "text/text", "Did not get battle name, fail")
+			ServerMutex.Unlock()
+			return
+		}
+		femap, ok := TheFireEvents[battleName]
+		log.Printf("Femap: %v Ok: %v", femap, ok)
+		if !ok || femap == nil {
+			log.Printf("Battle maps nil")
+			c.String(422, "text/text", "Battle map nil")
+			ServerMutex.Unlock()
+			return
+		}
+		c.JSON(http.StatusOK, femap)
 		ServerMutex.Unlock()
 	})
 
